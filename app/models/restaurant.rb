ca0ca8cc05fe6b5d11096146bdd73607f0931803
Ami Onodera <onodera.camila@gmail.com>
2020-05-20 14:58:51 +0200

    solved challenge, looking decent

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
-  validates :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
