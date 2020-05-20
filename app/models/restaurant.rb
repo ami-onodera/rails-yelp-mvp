ac0b1322697a5ecc089ce5728f5dbbdbb83324a7
Ami Onodera <onodera.camila@gmail.com>
2020-05-20 13:28:16 +0200

    created reviews model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :category, presence: true
 end
